be2e68c70b25d9ec339fe8f86e6f1b668f74160c
Courtney Phillips <courtney.mae.phillips@gmail.com>
2018-07-10 09:39:09 -0700

    Squash bug in userClick() method.

--- scripts.js
+++ scripts.js
@@ -163,7 +163,7 @@ window.onload = function() {
 
 // CLICK LISTENERS
 const userClick = () => {
-  if (store.getState().songsById.arrayPosition === store.getState().songsById[store.getState().currentSongId].length - 1) {
+  if (store.getState().songsById[store.getState().currentSongId].arrayPosition === store.getState().songsById[store.getState().currentSongId].songArray.length - 1) {
     store.dispatch({ type: 'RESTART_SONG',
                      currentSongId: store.getState().currentSongId });
   } else {
